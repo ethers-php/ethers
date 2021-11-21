f00446ef3e406aff1d9308fa6154b1ac66831d3c
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2021-11-21 19:50:54 +0200

    wip

--- src/EthersPHP/Bytes/Functions.php
+++ src/EthersPHP/Bytes/Functions.php
@@ -5,9 +5,6 @@ namespace EthersPHP\Bytes;
 use EthersPHP\Bytes\Concerns\Hexable;
 
 if (!function_exists('arrayify')) {
-    /**
-     * Sets the renderer implementation.
-     */
     function arrayify(int|string|array|Hexable $data): array
     {
         return Bytes::arrayify($data);
@@ -16,11 +13,15 @@ if (!function_exists('arrayify')) {
 
 
 if (!function_exists('isHexString')) {
-    /**
-     * Sets the renderer implementation.
-     */
     function isHexString(mixed $value, $length = null): bool
     {
         return Bytes::isHexString($value, $length);
+    }
+}
+
+if (!function_exists('isBytes')) {
+    function isBytes(mixed $value): bool
+    {
+        return Bytes::isBytes($value);
     }
 }
